378ecba5e0d182c2650f9a867705ac738a9e4be3
Rachel Dorn <rachel.m.dorn@gmail.com>
2015-12-18 09:39:31 -0500

    Initial Commit

---
+++ DiceRoller/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DiceRoller
+//
+//  Created by Rachel Dorn on 12/18/15.
+//  Copyright © 2015 Rachel Dorn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
